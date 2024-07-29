872fc2e0ec0a0295bb92e0b1dfe822cac5f3b1b1
Eldar <eldaro@bicomsystems.com>
2024-07-30 01:08:12 +0200

    login

--- src/main/java/grupa5/MainScreenController.java
+++ src/main/java/grupa5/MainScreenController.java
@@ -2,14 +2,47 @@ package grupa5;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class MainScreenController {
      @FXML
     private Label testLabel;
 
+    private Stage loginStage; // Referenca na prozor za prijavu
+
     @FXML
     void testBtnClicked(ActionEvent event) {
-        testLabel.setText("radi");
+         try {
+            if (loginStage == null || !loginStage.isShowing()) {
+                // Učitajte login.fxml
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("views/login.fxml"));
+                Parent root = fxmlLoader.load();
+
+                // Kreirajte novi Stage
+                loginStage = new Stage();
+                loginStage.setTitle("Login Window");
+                loginStage.setScene(new Scene(root, 800, 600));
+                loginStage.setResizable(false); // Onemogućite promenu veličine
+
+                // Postavite modalnost
+                loginStage.initModality(Modality.APPLICATION_MODAL);
+
+                // Prikažite prozor
+                loginStage.showAndWait(); // showAndWait() čeka da se prozor zatvori
+
+                // Dodajte slušaoca događaja za zatvaranje prozora
+                loginStage.setOnCloseRequest(e -> loginStage = null);
+            } else {
+                // Fokusirajte prozor ako je već otvoren
+                loginStage.toFront();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
