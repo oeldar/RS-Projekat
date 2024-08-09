2ffe7b358d5699b40632eea9a3a47fc42aaf99c0
Eldar <eldaro@bicomsystems.com>
2024-08-09 14:12:15 +0200

    events dinamicki mijenja

--- src/main/java/grupa5/EventCardController.java
+++ src/main/java/grupa5/EventCardController.java
@@ -1,17 +1,26 @@
 package grupa5;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+
+import java.io.IOException;
+
 import grupa5.DogadjajMoj;
 
 public class EventCardController {
+    DogadjajMoj dogadjajMojTrenutni;
 
     @FXML
     private Text nazivText;
 
     @FXML
     private Text datumText;
+
+    @FXML
+    private ImageView eventImageView;
 
     private MainScreenController mainScreenController;
 
@@ -20,15 +29,19 @@ public class EventCardController {
     }
 
     public void setDogadjajMoj(DogadjajMoj dogadjajMoj) {
+        dogadjajMojTrenutni = dogadjajMoj;
         nazivText.setText(dogadjajMoj.getNaziv());
         datumText.setText(dogadjajMoj.getDatum().toString());
+        Image eventImage = new Image(getClass().getResource(dogadjajMoj.getImagePath()).toString());
+        eventImageView.setImage(eventImage);
+
     }
 
-    public void eventClicked(MouseEvent event) {
+    public void eventClicked(MouseEvent event) throws IOException {
         // Ovdje ide kod koji želite da se izvrši
         System.out.println("AnchorPane clicked!");
         if (mainScreenController != null) {
-            mainScreenController.loadDogadjaj1View(); // ili drugi view koji želite
+            mainScreenController.loadEventView(dogadjajMojTrenutni); // ili drugi view koji želite
         }
     }
     
--- src/main/java/grupa5/EventDetailsController.java
+++ src/main/java/grupa5/EventDetailsController.java
@@ -3,6 +3,8 @@ package grupa5;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class EventDetailsController {
     @FXML
@@ -11,6 +13,19 @@ public class EventDetailsController {
     @FXML
     public void initialize() {
         opisDogadjajaLbl.setWrapText(true);
+    }
+
+    @FXML
+    private Label eventTitle, eventDate;
+
+    @FXML
+    private ImageView eventImageView;
+
+    public void setDogadjaj(DogadjajMoj dogadjajMoj) {
+        eventTitle.setText(dogadjajMoj.getNaziv());
+        eventDate.setText(dogadjajMoj.getDatum().toString());
+        Image eventImage = new Image(getClass().getResource(dogadjajMoj.getImagePath()).toString());
+        eventImageView.setImage(eventImage);
     }
     
 
