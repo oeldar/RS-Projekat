55e1d6e8b607fe59d7ba827bc72310806aebd079
ivanmijic <ivan.mijic@fet.ba>
2024-08-22 08:35:41 +0200

    dodao nesto u bazu da provjerim

--- rs.session.sql
+++ rs.session.sql
@@ -1,23 +1,2 @@
-SELECT * FROM Rezervacije;
-
-delete FROM Rezervacije where ukupnaCijena = 0;
-
-SELECT * FROM Karte;
-
-UPDATE Karte SET dostupneKarte = 4;
-
-delete FROM Rezervacije;
-
-delete FROM Kupovine;
-
-SELECT * FROM Kupovine;
-
-SELECT * FROM Popusti;
-
-SELECT * FROM Dogadjaji;
-
-SHOW index from Rezervacije;
-
-SHOW index from Kupovine;
-
-SHOW index from Dogadjaji;
+INSERT INTO Karte (kartaID, cijena, dostupneKarte, maxBrojKartiPoKorisniku, naplataOtkazivanja, status, uslovOtkazivanja, dogadjajID, sektorID, brojKupljenih, brojRezervisanih) VALUES
+(24, 20.1, 100, 5, 0, 'DOSTUPNA', '254 sata pre događaja', 13, 8, 0, 0);
