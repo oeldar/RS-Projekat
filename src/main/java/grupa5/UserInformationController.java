a1c6b146bf2cc2937ad286b4cb5b197bbe7d5260
dulkaosmanovic <dulka.osmanovic@fet.ba>
2024-09-04 18:53:57 +0200

    rijesila se upozorenja

--- src/main/java/grupa5/UserInformationController.java
+++ src/main/java/grupa5/UserInformationController.java
@@ -12,7 +12,6 @@ import jakarta.persistence.Persistence;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.image.Image;
